ca62ca9d962587d2cd34ec882cf5e16682c22257
chester911 <chester911@nate.com>
2022-06-27 16:25:48 +0900

    통합 테이블 변경, 쿼리 변경

--- Reataurant/zomato.sql
+++ Reataurant/zomato.sql
@@ -32,7 +32,7 @@ order by 1;
 -- 통화 통일 (달러)
 select distinct currency
 from zomato;
-
+-- 매장별 지역, 분야, 가격, 평점 테이블 생성
 select `restaurant name` as 'restaurant',
 	cuisines,
 	city,
@@ -53,5 +53,30 @@ select `restaurant name` as 'restaurant',
         `aggregate rating`
 from zomato;
 
-select *
+# 배달여부 테이블 생성
+-- 위 테이블에 배달 여부 항목 추가
+-- 이 테이블을 태블로에 사용
+select `restaurant name`,
+	`country code`,
+    city,
+    longitude as 'long',
+    latitude as 'lat',
+    cuisines,
+	`has table booking` as 'booking',
+	`has online delivery` as 'online_delivery',
+	`is delivering now` as 'delivery',
+	case when currency like 'Botswana%' then `average cost for two`* 0.082
+		when currency like 'Brazilian%' then `average cost for two`* 0.19
+        when currency like 'Emirati%' then `average cost for two`* 0.27
+        when currency like 'Indian%' then `average cost for two`* 0.013
+        when currency like 'Indonesian%' then `average cost for two`* 0.000068
+        when currency like 'NewZealand%' then `average cost for two`* 0.63
+        when currency like 'Pounds%' then `average cost for two`* 1.23
+        when currency like 'Qatari%' then `average cost for two`* 0.27
+        when currency like 'Rand%' then `average cost for two`* 0.063
+        when currency like 'Sri%' then `average cost for two`* 0.0028
+        when currency like 'Turkish%' then `average cost for two`* 0.062
+			else `average cost for two`
+	end as 'price',
+    `aggregate rating` as 'rating'
 from zomato;
