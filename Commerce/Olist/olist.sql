f03a10504a55b68c4f69e54f9e29eda9f630c5b7
chester911 <chester911@nate.com>
2022-06-27 22:43:59 +0900

    지역별 테이블 추가

--- Commerce/Olist/olist.sql
+++ Commerce/Olist/olist.sql
@@ -13,3 +13,49 @@ from olist.orders as a
 		on a.order_id= c.order_id
 group by 1, 2, 3, 4
 order by 1;
+
+# 지역별 매출
+-- 위도, 경도 포함
+-- unavailable, canceled 주문 제외
+with monthly as
+(select substr(a.order_purchase_timestamp, 1, 7) as YM,
+	c.customer_state as 'state',
+	sum(b.price) as rev
+from olist.orders as a
+	left join olist.order_items as b
+		on a.order_id= b.order_id
+	left join olist.customers as c
+		on a.customer_id= c.customer_id
+where a.order_status not in ('unavailable', 'canceled')
+group by 1, 2
+order by 1),
+
+maps as
+(select geolocation_state as 'state',
+	geolocation_city as 'city',
+	avg(geolocation_lat) as 'lat',
+    avg(geolocation_lng) as 'long'
+from olist.geolocation
+group by 1)
+
+select a.*, b.city, b.lat, b.long
+from monthly as a
+	left join maps as b
+		on a.state= b.state;
+
+# 지역별 최다 판매 물품 (주문 수, 매출)
+select substr(a.order_purchase_timestamp, 1, 7) as YM,
+	c.customer_state as 'state',
+    d.product_category_name as 'product',
+    count(distinct a.customer_id) as pu_cnt,
+    sum(b.price) as rev
+from olist.orders as a
+	left join olist.order_items as b
+		on a.order_id= b.order_id
+	left join olist.customers as c
+		on a.customer_id= c.customer_id
+	left join olist.products as d
+		on b.product_id= d.product_id
+where a.order_status not in ('unavailable', 'canceled')
+group by 1, 2, 3
+order by 1;
